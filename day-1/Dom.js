1c15c3ab8397cf86c2b89babdf7a16882559363b
monoronjon sarker <monoronjons86@gmail.com>
2023-04-08 17:25:26 +0600

    gettag

--- day-1/Dom.js
+++ day-1/Dom.js
@@ -1,1 +1,13 @@
-console.log('hello from js')
+// const student={
+//     name:"monoronjon sarker",
+//     ID:11,
+//     study:function(time){
+//         console.log(time,'study kortaco');
+//     }
+// }
+
+const liCollection=document.getElementsByTagName('li');
+console.log(liCollection);
+for(const li of liCollection){
+    
+}
