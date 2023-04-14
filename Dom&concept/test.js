13564da453bf88242eac0367ca981168a434af76
monoronjon sarker <monoronjons86@gmail.com>
2023-04-14 07:37:01 +0600

    tageNameUse

---
+++ Dom&concept/test.js
@@ -0,0 +1,10 @@
+// using tagname
+//  const tagList=document.getElementsByTagName('h1');
+// // for(const tag of tagList){
+// //     tag.style.color='red';
+
+// // }
+
+// for(let i=0;i<tagList.length;i++){
+//     console.log(tagList[i].innerText);
+// }
