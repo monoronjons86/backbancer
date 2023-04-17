5374285ce8aa30405f2b4204d5e62637fc3fc8fb
monoronjon sarker <monoronjons86@gmail.com>
2023-04-17 11:09:40 +0600

    event handlar finished

---
+++ Dom&concept/even.js
@@ -0,0 +1,9 @@
+function makeRed(){
+    document.body.style.backgroundColor='red';
+    document.getElementById('heading').innerText='testing dom events';
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+    document.getElementById('hand').innerText='with dom';
+}
