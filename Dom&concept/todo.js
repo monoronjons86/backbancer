f7b0b5a00bdf0c3b2e28fa53e64ae2677c105fbf
monoronjon sarker <monoronjons86@gmail.com>
2023-04-18 21:29:14 +0600

    buji na

--- Dom&concept/todo.js
+++ Dom&concept/todo.js
@@ -1,4 +1,30 @@
+let count=0;
 document.getElementById('input-btn').addEventListener('click',function(){
     const myInputValue=document.getElementById('input-value').value;
-    console.log(myInputValue);
+    // console.log(myInputValue);
+    count +=1;
+
+    const parent=document.getElementById('content-container');
+    const tr=document.createElement('tr');
+    tr.innerHTML=`
+    <td>${count}</td>
+    <td>${myInputValue}</td>
+    <td>
+     <button class="btn btn-danger DeleteBtn">Delete</button>
+     <button class="btn btn-primary">Done</button>
+    </td>
+    `
+    parent.appendChild(tr);
+    document.getElementById('input-value').value=' ';
+
+const elements=document.querySelectorAll('.DeleteBtn');
+ console.log(elements)
+  for(const value of elements)
+  {
+    value.addEventListener('click',function(event){
+        console.log(event.target.innerText);
+
+    });
+  }
 });
+
