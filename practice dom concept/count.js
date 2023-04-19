59c9f22ec64fa3503a7a0c363a044d04c52f571d
monoronjon sarker <monoronjons86@gmail.com>
2023-04-19 16:19:56 +0600

    count finished

--- practice dom concept/count.js
+++ practice dom concept/count.js
@@ -3,3 +3,8 @@ document.getElementById('primary').addEventListener('click',function(){
     value+=1;
 document.getElementById('counter').innerText=value;
 });
+
+document.getElementById('danger').addEventListener('click',function(){
+    value-=1;
+    document.getElementById('counter').innerText=value;
+});
