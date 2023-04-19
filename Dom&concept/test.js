49c4780021584fcc4edb1207836711a2c07108d9
monoronjon sarker <monoronjons86@gmail.com>
2023-04-19 20:40:47 +0600

    getelementadd

--- Dom&concept/test.js
+++ Dom&concept/test.js
@@ -1,9 +1,9 @@
 // using tagname
 //  const tagList=document.getElementsByTagName('h1');
-// // for(const tag of tagList){
-// //     tag.style.color='red';
+//  for(const tag of tagList){
+//      tag.style.color='red';
 
-// // }
+//  }
 
 // for(let i=0;i<tagList.length;i++){
 //     console.log(tagList[i].innerText);
---
+++ practice/abul.js
@@ -0,0 +1,5 @@
+// using tagname
+const tagList=document.getElementsByTagName('h1');
+for(const tag of tagList){
+    tag.style.color='yellow';
+}
