14f8021ce7b9e2254cef6cd65515ef0536367c20
monoronjon sarker <monoronjons86@gmail.com>
2023-04-23 09:02:09 +0600

    deposit

--- Baap ar bank/dp.js
+++ Baap ar bank/dp.js
@@ -1,4 +1,14 @@
+//step-1:add event listener to the deposit button
 document.getElementById('btn-deposit').addEventListener('click',function(){
-    console.log('clicked me');
+    //step-2:get the deposit amount form the deposit input filed
+    //for  input field use .value to the value inside the input field
+    const depositFiled= document.getElementById('deposit-field');
+    const depositAmount=depositFiled.value ;
+    
+    //step-3:get the current deposit total amount
+    const depositTotalElement=document.getElementById('deposit-total');
+    const depositTotal=depositTotalElement.innerText;
+    depositTotalElement.innerText=depositAmount;
+
 
 });
