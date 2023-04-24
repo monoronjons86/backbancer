217813cb3f4131b1522db0c7eb2802cff14b4f22
monoronjon sarker <monoronjons86@gmail.com>
2023-04-24 21:12:30 +0600

    user deposit

---
+++ Baap ar bank recap/Deposit.js
@@ -0,0 +1,20 @@
+/*
+1.add event listener to the deposit button
+2-5 .convert string deposit amount to a number type
+3.clear the deposit input field after getting the value 
+4.get the previous deposit total
+*/
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const DepositField=document.getElementById('deposit-filed');
+    const DepositFieldString=DepositField.value;
+    const newDepositAmount=parseFloat(DepositFieldString);
+    DepositField.value=' ';
+
+    const DepositTotalElement=document.getElementById('deposit-total');
+    const previousDepositTotalString=DepositTotalElement.innerText;
+    const previousDepositTotal=parseFloat(previousDepositTotalString);
+
+    const newDepositTotal=previousDepositTotal+newDepositAmount;
+    DepositTotalElement.innerText=newDepositTotal;
+
+});
