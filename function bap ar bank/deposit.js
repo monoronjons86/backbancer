3351ddfc64539a34864b801268d1c3c004c8c01c
monoronjon sarker <monoronjons86@gmail.com>
2023-04-25 11:09:08 +0600

    set element function

--- function bap ar bank/deposit.js
+++ function bap ar bank/deposit.js
@@ -1,19 +1,24 @@
-function getInputVFiledValueById(inputId){
-    const inputField=document.getElementById(inputId);
-    const inputFieldValueString=inputField.value ;
-    const inputFieldValue=parseFloat(inputFieldValueString);
-    return inputFieldValue;
-}
-
-function getElementById(elementId){
-    const element=document.getElementById(elementId);
-    const elementValueString=element.innerText;
-    const value=parseFloat(elementValueString);
-    return value;
-
-}
 document.getElementById('btn-deposit').addEventListener('click',function(){
-    const newDepositAmount=getInputVFiledValueById('deposit-field')
+    /*
+    1.get the element by id
+    2.get the value form the element
+    3.convert to string value to a number
+    */
+    const newDepositAmount=getInputVFiledValueById('deposit-filed')
+    /*
+    get previous deposit total by id
+    */
+   const previousDepositTotal=getTextElementValueById('deposit-total');
+   //calculate new deposit total
+   const newDepositTotal=previousDepositTotal+newDepositAmount
+   //set deposit total value
+   setTextElementValueById('deposit-total',newDepositTotal);
+   
+   //get previous balance by using the function
+   const previousBalanceTotal=getTextElementValueById('balance-total');
+   const newBalanceTotal=previousBalanceTotal+newDepositAmount;
+   setTextElementValueById('balance-total',newBalanceTotal)
+
     
 
 });
---
+++ function bap ar bank/utilites.js
@@ -0,0 +1,24 @@
+function getInputVFiledValueById(inputId){
+    const inputField=document.getElementById(inputId);
+    const inputFieldValueString=inputField.value ;
+    const inputFieldValue=parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
+
+function getElementById(elementId){
+    const element=document.getElementById(elementId);
+    const elementValueString=element.innerText;
+    const value=parseFloat(elementValueString);
+    return value;
+
+}
+function getTextElementValueById(elementId){
+    const textElement=document.getElementById(elementId);
+    const textElementValueString=textElement.innerText;
+    const textElementValue=parseFloat(textElementValueString);
+    return textElementValue;
+}
+function setTextElementValueById(elementId,newValue){
+    const textElement=document.getElementById(elementId);
+    textElement.innerText=newValue;
+}
