673a7ca8991d97e16e133e47b1fcd478facd8352
monoronjon sarker <monoronjons86@gmail.com>
2023-04-27 01:17:19 +0600

    night-finished

--- function bap ar bank/deposit.js
+++ function bap ar bank/deposit.js
@@ -1,9 +1,15 @@
+function getInputFiledValueById(inputId){
+    const inputField=document.getElementById('deposit-filed');
+    const inputFieldValueString=inputField.value;
+    const inputFieldValue=parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
 document.getElementById('btn-deposit').addEventListener('click',function(){
     /*
     1.get the element by id
     2.get the value form element
     3.convert string  value to a number
-    
     */
+    
 
 });
