52bf840c46bf64fc49c9520feaba2b2fff5ffbec
monoronjon sarker <monoronjons86@gmail.com>
2023-04-26 10:11:49 +0600

    morning start

---
+++ function bap ar bank/withdraw.js
@@ -0,0 +1,7 @@
+/*
+1.add withdraw button event handler
+2.get withdraw amount by using getinputvalueid 
+*/
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    console.log('withdraw button clicked');
+});
