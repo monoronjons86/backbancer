e0cb8374aad6df32459b8b5d020ba17896d61cd2
monoronjon sarker <monoronjons86@gmail.com>
2023-04-27 00:12:17 +0600

    again see and coding

--- function bap ar bank/deposit.js
+++ function bap ar bank/deposit.js
@@ -1,24 +1,9 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
     /*
     1.get the element by id
-    2.get the value form the element
-    3.convert to string value to a number
-    */
-    const newDepositAmount=getInputVFiledValueById('deposit-filed')
-    /*
-    get previous deposit total by id
-    */
-   const previousDepositTotal=getTextElementValueById('deposit-total');
-   //calculate new deposit total
-   const newDepositTotal=previousDepositTotal+newDepositAmount
-   //set deposit total value
-   setTextElementValueById('deposit-total',newDepositTotal);
-   
-   //get previous balance by using the function
-   const previousBalanceTotal=getTextElementValueById('balance-total');
-   const newBalanceTotal=previousBalanceTotal+newDepositAmount;
-   setTextElementValueById('balance-total',newBalanceTotal);
-
+    2.get the value form element
+    3.convert string  value to a number
     
+    */
 
 });
