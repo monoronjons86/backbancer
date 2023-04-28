13a4428c3beeb3d4e53d360ab05e6843912ed2d7
monoronjon sarker <monoronjons86@gmail.com>
2023-04-28 22:03:44 +0600

    finished

--- function bap ar bank/withdraw.js
+++ function bap ar bank/withdraw.js
@@ -3,11 +3,18 @@
 2.get withdraw amount by using getInputFieldValueById function
 3.get previous withdraw amount by using getTextElementValueById function
 4.calculate new withdraw total and set the value
+4-5.set new withdraw total by using setTextElementValueById function
+5.get the previous balance total by using getTextElementValue 
+6.calculate new balance total  
+7.set balance total using setTextElementValueById
 */
 document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithdrawAmount=getInputFiledValueById('withdraw-field');
     const previousWithdrawTotal=getTextElementValueById('withdraw-total');
-    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount
+    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
+    setTextElementValueById('withdraw-total',newWithdrawTotal);
+    const previousBalanceTotal=getTextElementValueById('balance-total')
+    const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
+    setTextElementValueById('balance-total',newBalanceTotal);
 
-    
 });
