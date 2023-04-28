0bacf151b67a20f3d8df49c5d529519b0cd04a56
monoronjon sarker <monoronjons86@gmail.com>
2023-04-28 21:00:59 +0600

    today-start

--- function bap ar bank/withdraw.js
+++ function bap ar bank/withdraw.js
@@ -1,3 +1,13 @@
+/*
+1.add withdraw button event handler
+2.get withdraw amount by using getInputFieldValueById function
+3.get previous withdraw amount by using getTextElementValueById function
+4.calculate new withdraw total and set the value
+*/
 document.getElementById('btn-withdraw').addEventListener('click',function(){
-    console.log('clicked here');
+    const newWithdrawAmount=getInputFiledValueById('withdraw-field');
+    const previousWithdrawTotal=getTextElementValueById('withdraw-total');
+    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount
+
+    
 });
