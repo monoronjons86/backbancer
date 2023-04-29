b1d2f529c8583229ac41005db7c8ce7a18bd3cfb
monoronjon sarker <monoronjons86@gmail.com>
2023-04-29 22:20:47 +0600

    id aded

--- Dom-2/index.js
+++ Dom-2/index.js
@@ -3,7 +3,15 @@
 //     tag.style.color='red';
 // }
 
-const classList=document.getElementsByClassName('test');
-for(const value of classList){
-    value.style.BackgroundColor='red';
-}
+// const classList=document.getElementsByClassName('test');
+// for(const value of classList){
+//     value.style.BackgroundColor='red';
+// }
+// const myHeading=document.getElementById('heading');
+// myHeading.style.textAlign='center';
+// myHeading.style.color='red';
+// myHeading.style.backgroundColor='green';
+// console.log(myHeading.innerText);
+
+document.getElementById('heading').style.color='yellow';
+document.getElementById('heading').style.backgroundColor='tomato';
