2d7ad194d9b7a89e47a6ef313fd9cb061467d187
monoronjon sarker <monoronjons86@gmail.com>
2023-04-29 09:16:46 +0600

    morning start

--- function bap ar bank/utilites.js
+++ function bap ar bank/utilites.js
@@ -1,10 +1,10 @@
-function getInputFiledValueById(inputFieldId){
-    const inputField=document.getElementById(inputFieldId);
-    const inputFieldValueString=inputField.value;
-    const inputFieldValue=parseFloat(inputFieldValueString);
-    inputField.value='';
-    return inputFieldValue;
-}
+// function getInputFiledValueById(inputFieldId){
+//     const inputField=document.getElementById(inputFieldId);
+//     const inputFieldValueString=inputField.value;
+//     const inputFieldValue=parseFloat(inputFieldValueString);
+//     inputField.value='';
+//     return inputFieldValue;
+// }
 function getTextElementValueById(elementId){
     const textElement=document.getElementById(elementId);
     const textElementValueString=textElement.innerText;
@@ -15,4 +15,7 @@ function setTextElementValueById(elementId,newValue){
     const textElement=document.getElementById(elementId);
     textElement.innerText=newValue;
 
+}
+function getInputFieldValueById(inputField){
+    co
 }
