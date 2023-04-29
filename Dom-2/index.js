49de55039da82276c054449bfd11608c315b3d45
monoronjon sarker <monoronjons86@gmail.com>
2023-04-29 21:44:02 +0600

    element

---
+++ Dom-2/index.js
@@ -0,0 +1,9 @@
+// const tagList=document.getElementsByTagName('h1');
+// for(const tag of tagList){
+//     tag.style.color='red';
+// }
+
+const classList=document.getElementsByClassName('test');
+for(const value of classList){
+    value.style.BackgroundColor='red';
+}
