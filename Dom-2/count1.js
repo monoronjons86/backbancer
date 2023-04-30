7c6cec6fc5e335ed5956e348a38cc820d168bc75
monoronjon sarker <monoronjons86@gmail.com>
2023-04-30 20:15:33 +0600

    count finished

--- Dom-2/count1.js
+++ Dom-2/count1.js
@@ -3,7 +3,18 @@ document.getElementById('btn-danger').addEventListener('click',function(){
     value=value+1
     document.getElementById('counter').innerText=value;
 });
-function makeMinus(){
+
+document.getElementById('btn-success').addEventListener('click',function(){
     value-=1
     document.getElementById('counter').innerText=value;
-}
+
+});
+
+
+
+
+
+// function makeMinus(){
+//     value-=1
+//     document.getElementById('counter').innerText=value;
+// }
