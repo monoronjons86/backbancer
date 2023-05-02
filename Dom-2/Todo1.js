3506858aec1bbaef26ef71578feff053fe5cb003
monoronjon sarker <monoronjons86@gmail.com>
2023-05-02 20:12:26 +0600

    today start

--- Dom-2/Todo1.js
+++ Dom-2/Todo1.js
@@ -1,17 +1,27 @@
+let count=0;
 document.getElementById('input-btn').addEventListener('click',function(){
-    const myInputValue=document.getElementById('input-value').value;
+    count+=1;
+    const myInputValue=document.getElementById('input-value');
+    const myInput=myInputValue.value ;
+    myInputValue.value='';
 
     const parent=document.getElementById('content-contaniner');
     const tr=document.createElement('tr');
     tr.innerHTML=`
-    <td>1</td>
-    <td>${myInputValue}</td>
+    <td>${count}</td>
+    <td>${myInput}</td>
     <td>
-    <button>Delete</button>
-    <button>Done</button>
+    <button class='btn btn-danger DeleteBtn'>Delete</button>
+    <button class='btn btn-primary'>Done</button>
     </td>
 
     `
     parent.appendChild(tr);
+    const elements= document.querySelectorAll('.DeleteBtn')
+   for(const value of elements){
+    value.addEventListener('click',function(){
+        console.log('delete button clicked')
+    })
+    }
 
 });
