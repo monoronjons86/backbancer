948d7684b9f25389c71a3841e8302003c442935e
monoronjon sarker <monoronjons86@gmail.com>
2023-05-03 06:33:49 +0600

    hojorbol

---
+++ Dom-2-practice/index.js
@@ -0,0 +1,18 @@
+// const tagList=document.getElementsByTagName('h1');
+// for(const tag of tagList){
+//     tag.style.color='red';
+// }
+
+// const classList=document.getElementsByClassName('test');
+// for(const list of classList ){
+//     list.style.color='green';
+// }
+
+// const heading=document.getElementById('heading');
+// heading.style.color='green';
+// heading.style.textAlign='center';
+// heading.style.padding='20px';
+// heading.style.fontSize='50px';
+
+
+
