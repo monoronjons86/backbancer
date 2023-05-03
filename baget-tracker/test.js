77665e02d232a6648c2660793449682f9b300ee9
monoronjon sarker <monoronjons86@gmail.com>
2023-05-03 21:18:53 +0600

    baget tracker add

---
+++ baget-tracker/test.js
@@ -0,0 +1,8 @@
+document.getElementById('add-btn').addEventListener('click',function(){
+    const value=document.getElementById('text-input').value;
+    document.getElementById('text-input').value='';
+    const container=document.getElementById('parent-container');
+    const li=document.createElement('li');
+    li.innerText=value;
+    container.appendChild(li);
+});
