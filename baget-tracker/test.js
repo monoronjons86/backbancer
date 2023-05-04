70480aa065e9c9775b249d53f174888d2198ed07
monoronjon sarker <monoronjons86@gmail.com>
2023-05-04 17:15:04 +0600

    learn function finishid

--- baget-tracker/test.js
+++ baget-tracker/test.js
@@ -13,3 +13,20 @@ document.getElementById('add-btn').addEventListener('click',function(){
         });
     }
 });
+document.getElementById('hit').addEventListener('click',function(){
+setInnerText('p-1','bangladesh');
+setInnerText('p-2','india');
+setInnerText('p-3','pakistan');
+setInnerText('p-4','canada');
+const inputValue= getInputValue('text-input');
+console.log(inputValue);
+
+});
+function setInnerText(id,value){
+    document.getElementById(id).innerText='bangladesh'.innerText=value;
+
+}
+function getInputValue(id){
+   const value= document.getElementById(id).value;
+   return value;
+}
