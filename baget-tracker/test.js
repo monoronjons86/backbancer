c2b51666f426cde4792768b42d095fb8e924a793
monoronjon sarker <monoronjons86@gmail.com>
2023-05-04 10:41:09 +0600

    morning start

--- baget-tracker/test.js
+++ baget-tracker/test.js
@@ -9,9 +9,7 @@ document.getElementById('add-btn').addEventListener('click',function(){
     const allList=document.getElementsByClassName('new-li');
     for(const item of allList){
         item.addEventListener('click',function(e){
-            console.log(e.target.parentNode);
-
-
+            e.target.parentNode.removeChild(e.target)
         });
     }
 });
