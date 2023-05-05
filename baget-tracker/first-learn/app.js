755b4f188f43ba7831ead45a16e339f7f2855660
monoronjon sarker <monoronjons86@gmail.com>
2023-05-05 23:43:48 +0600

    total-add

--- baget-tracker/first-learn/app.js
+++ baget-tracker/first-learn/app.js
@@ -1,17 +1,35 @@
 document.getElementById('kit-katbuy-btn').addEventListener('click',function(){
-   const quantity=document.getElementById('kitkat-quantity').value;
-   const kitkatCost=quantity*200;
-   document.getElementById('chocolate').innerText=kitkatCost;
+   const quantity=getInputValue('kitkat-quantity')
+   const kitkat=quantity*100
+   setInnerText('chocolate',kitkat)
+   total()
 });
 
 document.getElementById('rose-buy-btn').addEventListener('click',function(){
-    const quantity=document.getElementById('rose-quantity').value;
+    const quantity=getInputValue('rose-quantity')
     const RoseCost=quantity*200;
-    document.getElementById('rose').innerText=RoseCost;
+    setInnerText('rose',RoseCost);
+    total()
  });
  document.getElementById('dairy-btn').addEventListener('click',function(){
     const quantity=document.getElementById('dairy-quantity').value;
     const dairyCost=quantity*200;
-    document.getElementById('dairy').innerText=dairyCost;
+    setInnerText('dairy',dairyCost);
+    total()
 
  });
+
+ function setInnerText(id,value){
+    document.getElementById(id).innerText=value
+ }
+ function getInputValue(id){
+    const quantity=document.getElementById(id).value;
+    return parseInt(quantity);
+ }
+ function total(){
+    const chocolate=document.getElementById('chocolate').innerText;
+    const rose=document.getElementById('rose').innerText;
+    const dairy=document.getElementById('dairy').innerText;
+    const totalSum=parseInt(chocolate)+parseInt(rose)+parseInt(dairy);
+    setInnerText('Total',totalSum);
+ }
