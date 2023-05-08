e2acc825ab64c633eacc97638715abf5e91c640b
monoronjon sarker <monoronjons86@gmail.com>
2023-05-08 11:52:16 +0600

    increase

--- shopping-cart/case.js
+++ shopping-cart/case.js
@@ -6,17 +6,31 @@
 5.set the value to the case number field
 
 */
+function updatecaseNumber(isIncrease){
+    const caseNumberField= document.getElementById('case-number-filed');
+    const caseNumberString=caseNumberField.value;
+    const previousCaseNumber=parseInt(caseNumberString);
+ 
+    let newCaseNumber;
+    if(isIncrease===true){
+        newCaseNumber=previousCaseNumber+1
+    }
+    else{
+        newCaseNumber=previousCaseNumber-1;
+    }
+    caseNumberField.value=newCaseNumber;
+    return newCaseNumber
+
+}
 
 document.getElementById('btn-case-plus').addEventListener('click',function(){
-   const caseNumberField= document.getElementById('case-number-filed');
-   const caseNumberString=caseNumberField.value;
-   const previousCaseNumber=parseInt(caseNumberString);
-
-   const newCaseNumber=previousCaseNumber+1;
-   caseNumberField.value=newCaseNumber
+    const newCaseNumber= updatecaseNumber(true);
+    const caseTotalPrice=newCaseNumber*59;
+    const caseTotalElement=document.getElementById('case-total');
+    caseTotalElement.innerText=caseTotalPrice;
 
 });
 document.getElementById('btn-case-minus').addEventListener('click',function(){
-    const caseNumberField=document.getElementById()
+    updatecaseNumber(false)
 
 });
