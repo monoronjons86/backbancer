ac9b5cd75a9e04a2aea52fb94ded3e53e2d9f1dc
monoronjon sarker <monoronjons86@gmail.com>
2023-05-09 22:44:00 +0600

    increase add

--- shopping-cart/practice.js
+++ shopping-cart/practice.js
@@ -1,29 +1,61 @@
+// function updatecaseNumber(isIncrease){
+//     const caseNumberField=document.getElementById('case-number-filed');
+//     const caseNumberString=caseNumberField.value ;
+//     const previousCaseNumber=parseInt(caseNumberString);
+
+//     let newCaseNumber;
+//     if(isIncrease===true){
+//         newCaseNumber=previousCaseNumber+1;
+//     }
+//     else{
+//         newCaseNumber=previousCaseNumber-1;
+//     }
+//     caseNumberField.value=newCaseNumber;
+//     return newCaseNumber;
+
+// }
+// document.getElementById('btn-case-plus').addEventListener('click',function(){
+//     const newCaseNumber=updatecaseNumber(true);
+
+//     const caseTotalPrice=newCaseNumber*59;
+//     const caseTotalElement=document.getElementById('case-total');
+//     caseTotalElement.innerText=caseTotalPrice;
+
+
+// });
+// document.getElementById('btn-case-minus').addEventListener('click',function(){
+//     updatecaseNumber(false);
+
+// });
+
 function updatecaseNumber(isIncrease){
     const caseNumberField=document.getElementById('case-number-filed');
     const caseNumberString=caseNumberField.value ;
     const previousCaseNumber=parseInt(caseNumberString);
 
     let newCaseNumber;
-    if(isIncrease===true){
+    if(isIncrease==isIncrease){
         newCaseNumber=previousCaseNumber+1;
+
     }
     else{
         newCaseNumber=previousCaseNumber-1;
     }
-    caseNumberField.value=newCaseNumber;
+    caseNumberField.value =newCaseNumber;
     return newCaseNumber;
-
 }
+
+
 document.getElementById('btn-case-plus').addEventListener('click',function(){
     const newCaseNumber=updatecaseNumber(true);
-    
     const caseTotalPrice=newCaseNumber*59;
     const caseTotalElement=document.getElementById('case-total');
     caseTotalElement.innerText=caseTotalPrice;
 
 
 });
+
 document.getElementById('btn-case-minus').addEventListener('click',function(){
-    updatecaseNumber(false);
+  updatecaseNumber(false);
 
 });
