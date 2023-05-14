7c47e380ea44b9d01c6d20406b58515d3fe21759
monoronjon sarker <monoronjons86@gmail.com>
2023-05-14 23:39:23 +0600

    subtotal add

--- shopping-cart/practicePhone.js
+++ shopping-cart/practicePhone.js
@@ -19,12 +19,28 @@ function updatePhoneTotalPrice(newPhoneNumber){
     const phoneTotalElement=document.getElementById('phone-total');
     phoneTotalElement.innerText=phoneTotalPrice;
 }
+function getTextElementValueById(element){
+    const phoneTotalElement=document.getElementById(element);
+    const currentPhoneTotalString=phoneTotalElement.innerText;
+    const currentPhoneTotal=parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+}
+
 document.getElementById('btn-phone-plus').addEventListener('click',function(){
    const newPhoneNumber=updatePhoneNumber(true);
-   updatePhoneTotalPrice(newPhoneNumber)
+   updatePhoneTotalPrice(newPhoneNumber);
+
+   //calculate total
+   const currentPhoneTotal=getTextElementValueById('phone-total');
+   const currentCaseTotal=getTextElementValueById('case-total');
+   const currentSubTotal=currentPhoneTotal+currentCaseTotal;
+
+
+
 });
 document.getElementById('btn-phone-minus').addEventListener('click',function(){
    const newPhoneNumber=updatePhoneNumber(false);
+   updatePhoneTotalPrice(newPhoneNumber);
 
 
 });
