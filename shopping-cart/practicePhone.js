c7ea5b491509e8e9f24514494c6bfa31fa9d1177
monoronjon sarker <monoronjons86@gmail.com>
2023-05-14 12:53:24 +0600

    today start

--- shopping-cart/practicePhone.js
+++ shopping-cart/practicePhone.js
@@ -1,13 +1,24 @@
-function updatePhoneNumber(){
+function updatePhoneNumber(isIncrease){
     const phoneNumberField=document.getElementById('phone-number-filed');
     const phoneNumberString=phoneNumberField.value ;
     const previousPhoneNumber=parseInt(phoneNumberString);
 
-    const newPhoneNumber=previousPhoneNumber+1;
+    let newPhoneNumber;
+    if(isIncrease){
+        newPhoneNumber=previousPhoneNumber+1;
+    }
+    else{
+        newPhoneNumber=previousPhoneNumber-1;
+    }
     phoneNumberField.value=newPhoneNumber;
+    return newPhoneNumber;
 
 }
 document.getElementById('btn-phone-plus').addEventListener('click',function(){
+    updatePhoneNumber(true);
+});
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+    updatePhoneNumber(false);
 
 
 });
