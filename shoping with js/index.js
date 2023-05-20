bc569765cabf2dfa6e22866cf54dda719a0165b7
monoronjon sarker <monoronjons86@gmail.com>
2023-05-20 18:19:26 +0600

    add

--- shoping with js/index.js
+++ shoping with js/index.js
@@ -27,6 +27,7 @@ document.getElementById('first-card').addEventListener('click',function(){
     displayData(ProductName,ProductPrice,ProductQuantity,priceTotal);
 
 });
+//using second card
 //using event object from browser
 document.getElementById('second-card').addEventListener('click',function(e){
     serial+=1;
@@ -41,14 +42,18 @@ displayData(productName,productPrice,ProductQuantity,sumToTal);
 
 });
 
+// using third card
 document.getElementById('third-card').addEventListener('click',function(){
     // serial=serial+1;
     serial+=1;
     //get the data from html using id
+
+
+
     const ProductName=document.getElementById('third-title').innerText;
     const ProductPrice=document.getElementById('third-price').innerText;
     const ProductQuantity=document.getElementById('third-quantity').innerText;
-    // console.log(typeof ProductPrice,typeof ProductQuantity);
+     console.log(typeof ProductPrice,typeof ProductQuantity);
     const priceTotal=parseInt(ProductPrice)-parseInt(ProductQuantity);
     console.log(priceTotal, typeof priceTotal);
 
@@ -66,6 +71,12 @@ document.getElementById('third-card').addEventListener('click',function(){
 
     // using function
     displayData(ProductName,ProductPrice,ProductQuantity,priceTotal);
+
+});
+
+//using last card
+document.getElementById('last-card').addEventListener('click',function(){
+    console.log('clicked here');
 
 });
 
