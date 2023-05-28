34c7e70a23fb9f2145856a48ce030773318fa2ea
monoronjon sarker <monoronjons86@gmail.com>
2023-05-28 19:29:13 +0600

    recap add

---
+++ Dom-recap/recap.js
@@ -0,0 +1,17 @@
+/*
+1.how to use js with html and css
+2.get element or elements from dom
+    2-1:getElementById
+    2-2:getElementsByClassName
+    2-3:querySelectorAll
+
+3.dynamically add element to the dom:
+4.get and set the value from a dom element
+    4-5:use innerText (h1,p,h5,div);
+    4-2:use value(input,textArea);
+
+5.add event listnier(event handler)
+  5-1:click,focus,blur,onchange,keyup
+  5-2:add
+6.use function to reduce duplicate code
+*/
