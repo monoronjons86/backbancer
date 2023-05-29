8f9cff66df9ce03d2c5284fe6c5fc4718fc353e2
monoronjon sarker <monoronjons86@gmail.com>
2023-05-29 10:50:55 +0600

    try-catch end

---
+++ Dom-recap/tryCatch.js
@@ -0,0 +1,18 @@
+const day='day the din';
+console.log(day);
+console.log(25);
+// console.log(district);
+// let district='bBaria';
+// console.log(
+//     61
+// )
+try{
+    console.log('inside try');
+    console.log(student);
+}
+catch(error){
+    console.log(error);
+}
+finally{
+    console.log('finally done');
+}
