1496289f92111935db6295ff86ea0b349ba3212c
monoronjon sarker <monoronjons86@gmail.com>
2023-05-29 17:48:20 +0600

    validatin

---
+++ Dom-recap/validation.js
@@ -0,0 +1,12 @@
+const number=23
+if(typeof number==='number'){
+    console.log('value is number');
+}
+else{
+    console.log('value is not number');
+}
+const numbers=[3,12,78,90];
+const student={name:'mono',job:'khaya daya ar ghuma'}
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(student))
+console.log(typeof student);
